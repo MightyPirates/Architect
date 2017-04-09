67752e696a6dd4bad5ce06c8cedad6c10144daae
Florian Nücke <florian@nuecke.de>
2017-04-09 21:43:34 +0200

    Force an update on placed attached blocks to avoid them living where they shouldn't be able to.

--- src/main/java/li/cil/architect/common/converter/ConverterAttachedBlock.java
+++ src/main/java/li/cil/architect/common/converter/ConverterAttachedBlock.java
@@ -5,6 +5,10 @@ import li.cil.architect.common.config.Constants;
 import li.cil.architect.common.config.Settings;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public final class ConverterAttachedBlock extends AbstractConverterBase {
     public ConverterAttachedBlock() {
@@ -15,5 +19,12 @@ public final class ConverterAttachedBlock extends AbstractConverterBase {
     protected boolean canSerialize(final IBlockState state) {
         final Block block = state.getBlock();
         return Settings.isAttachedBlock(block) && super.canSerialize(state);
+    }
+
+    @Override
+    public void deserialize(final World world, final BlockPos pos, final Rotation rotation, final NBTBase data) {
+        super.deserialize(world, pos, rotation, data);
+
+        world.neighborChanged(pos, world.getBlockState(pos).getBlock(), pos);
     }
 }
