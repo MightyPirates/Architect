192d5667639367c6df125543d7df4b526d411dfd
Florian Nücke <florian@nuecke.de>
2017-04-26 00:06:42 +0200

    Fixed default key bindings.

--- src/main/java/li/cil/architect/client/KeyBindings.java
+++ src/main/java/li/cil/architect/client/KeyBindings.java
@@ -7,8 +7,8 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import org.lwjgl.input.Keyboard;
 
 public final class KeyBindings {
-    public static final KeyBinding rotateBlueprint = new KeyBinding(Constants.KEY_BINDINGS_BLUEPRINT_ROTATE, KeyConflictContext.IN_GAME, Keyboard.KEY_G, Constants.KEY_BINDINGS_CATEGORY_NAME);
-    public static final KeyBinding toggleGrid = new KeyBinding(Constants.KEY_BINDINGS_TOGGLE_GRID, KeyConflictContext.IN_GAME, Keyboard.KEY_R, Constants.KEY_BINDINGS_CATEGORY_NAME);
+    public static final KeyBinding rotateBlueprint = new KeyBinding(Constants.KEY_BINDINGS_BLUEPRINT_ROTATE, KeyConflictContext.IN_GAME, Keyboard.KEY_R, Constants.KEY_BINDINGS_CATEGORY_NAME);
+    public static final KeyBinding toggleGrid = new KeyBinding(Constants.KEY_BINDINGS_TOGGLE_GRID, KeyConflictContext.IN_GAME, Keyboard.KEY_G, Constants.KEY_BINDINGS_CATEGORY_NAME);
 
     public static void init() {
         ClientRegistry.registerKeyBinding(rotateBlueprint);
